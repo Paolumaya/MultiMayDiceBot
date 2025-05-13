3da50c235c7bb0da706486750cab513ab5cce9c7
Evan Johnston <evan@pfg.dom>
2025-05-13 22:00:28 +0000

    Lay out a sql database that should be adequate for any all multi

--- bot.py
+++ bot.py
@@ -1,15 +1,118 @@
+import sqlite3
+from typing import Optional
+
 import discord
 import tomlkit
+from discord import app_commands
+import traceback
+
+from discord.utils import MISSING
+
+
+class MultiClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+
+class Startup(discord.ui.Modal):
+    def __init__(self, *, title: str = MISSING, timeout: Optional[float] = None, custom_id: str = MISSING) -> None:
+        super().__init__(title=title, custom_id=custom_id, timeout=timeout)
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+        await interaction.response.send_message('Oops! Something went wrong. Report this to @paolumu.', ephemeral=True)
+
+        # Make sure we know what the error actually is
+        traceback.print_exception(type(error), error, error.__traceback__)
+
+
+class StartupCore(Startup, title='Core Body Parts'):
+    cnt_clone = discord.ui.TextInput(label="# of Individual Bodies", placeholder="enter a number", default="1")
+    cnt_torso = discord.ui.TextInput(label="# of Upper Bodies", placeholder="enter a number", default="1")
+    cnt_taurso = discord.ui.TextInput(label="# of Lower Bodies ('Taursos')", placeholder="enter a number", default="1")
+
+    async def on_submit(self, interaction: discord.Interaction):
+        print(self.cnt_clone)
+        print(self.cnt_torso)
+        print(self.cnt_taurso)
+        await interaction.response.send_message("Okay! Now run `/setup2`!", ephemeral=True)
+
+
+class StartupHead(Startup, title='Usually-Head-Related Parts'):
+    cnt_head = discord.ui.TextInput(label="# of Heads", placeholder="enter a number", default="1")
+    cnt_eye = discord.ui.TextInput(label="# of Eyes", placeholder="enter a number", default="2")
+    cnt_snout = discord.ui.TextInput(label="# of Snouts", placeholder="enter a number", default="1")
+    cnt_mouth = discord.ui.TextInput(label="# of Mouths", placeholder="enter a number", default="1")
+    cnt_tongue = discord.ui.TextInput(label="# of Tongues", placeholder="enter a number", default="1")
+
+    async def on_submit(self, interaction: discord.Interaction):
+        print(self.cnt_head)
+        print(self.cnt_eye)
+        print(self.cnt_snout)
+        print(self.cnt_mouth)
+        print(self.cnt_tongue)
+        await interaction.response.send_message("Almost Done! Now run `/setup3`!", ephemeral=True)
+
+
+class StartupExtremities(Startup, title='Extremities'):
+    cnt_arm = discord.ui.TextInput(label="# of Arms", placeholder="enter a number", default="2")
+    cnt_finger = discord.ui.TextInput(label="# of Fingers per hand", placeholder="enter a number", default="5")
+    cnt_leg = discord.ui.TextInput(label="# of Legs", placeholder="enter a number", default="2")
+    cnt_toe = discord.ui.TextInput(label="# of Toes per foot", placeholder="enter a number", default="5")
+
+    async def on_submit(self, interaction: discord.Interaction):
+        print(self.cnt_arm)
+        print(self.cnt_finger)
+        print(self.cnt_leg)
+        print(self.cnt_toe)
+        cur.execute()
+        await interaction.response.send_message(f'You should now be able to roll using `/roll`!', ephemeral=True)
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+MY_GUILD = discord.Object(id=245674056760819712)
+
+client = MultiClient(intents=intents)
+
+
+# client.tree = app_commands.CommandTree(client)
 
 
 @client.event
 async def on_ready():
     print("ready")
+
+
+@client.tree.command()
+async def setup1(interaction: discord.Interaction):
+    # x = await interaction.response.send_modal(StartupCore())
+    await interaction.response.send_modal(StartupCore())
+    # print(type(x))
+    # print(dir(x))
+    # print(x)
+
+
+@client.tree.command()
+async def setup2(interaction: discord.Interaction):
+    y = await interaction.response.send_modal(StartupHead())
+    print(type(y))
+    print(dir(y))
+    print(y)
+
+
+@client.tree.command()
+async def setup3(interaction: discord.Interaction):
+    z = await interaction.response.send_modal(StartupExtremities())
+    print(type(z))
+    print(dir(z))
+    print(z)
 
 
 def load_config() -> dict:
@@ -34,5 +137,30 @@ def load_config() -> dict:
             tomlkit.dump(default, cfg3)
         return default.copy()
 
+
 config = load_config()
+conn = sqlite3.connect(config["sql_file"])
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS multi (
+user_name TEXT
+user_id TEXT
+
+roll_cooldown INTEGER
+last_roll_timestamp INTEGER
+roll_count_column TEXT
+
+clones INTEGER
+torsos INTEGER
+taursos INTEGER 
+heads INTEGER
+eyes INTEGER
+snouts INTEGER
+mouths INTEGER
+tongues INTEGER
+arms INTEGER
+fingers INTEGER
+legs INTEGER
+toes INTEGER
+custom_things TEXT
+)""")
 client.run(config["token"])
--- multi.py
+++ multi.py
@@ -1,10 +1,51 @@
+import traceback
+
+import discord
 from discord.ext import commands
+import sqlite3
 
 
 class Multi(commands.Cog):
+    _sql_db: sqlite3.Connection
+
     def __init__(self, bot):
-        self.bot = bot
+        self.bot: commands.Bot = bot
+        self._sql_db: sqlite3.Connection = sqlite3.connect(bot.config)
 
     @commands.command()
-    async def roll(self, ctx: commands.Context):
+    async def start(self, ctx: commands.Context):
+        # check if person is registered already and ask if they want to reset
+        # otherwise, continue
         await ctx.reply("Not Yet Implemented")
+
+        # first_msg = await ctx.reply("Welcome to The MultiMay bot!\nWould you like to roll daily or weekly?")
+        # await first_msg.add_reaction(":one:")
+        # await first_msg.add_reaction(":seven:")
+        # rate = await self.bot.wait_for("on_reaction_add", check=lambda
+        #     x: x.id == first_msg.id and
+        #        x.channel.id == first_msg.channel.id and
+        #        x.author.id == ctx.author.id)
+        # second_msg = await ctx.reply("Okay, and how do you want to determine how many rolls you get?")
+
+    @commands.command()
+    async def roll(self, ctx: commands.Context, rolls):
+        await ctx.reply("Not Yet Implemented")
+
+    async def cog_load(self) -> None:
+        pass
+
+    async def cog_unload(self) -> None:
+        try:
+            self._sql_db.close()
+        except sqlite3.Error:
+            pass
+
+class Startup(discord.ui.Modal, title='MultiMay Setup'):
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f'You should now be able to roll!', ephemeral=True)
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+        await interaction.response.send_message('Oops! Something went wrong. Report this to @paolumu', ephemeral=True)
+
+        # Make sure we know what the error actually is
+        traceback.print_exception(type(error), error, error.__traceback__)
